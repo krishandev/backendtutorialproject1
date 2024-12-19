6eb3bbf08136ef538dec75f3232683977f4a4cdf
Krishan <krishandev.0102@gmail.com>
2024-12-19 06:12:43 +0530

    edit file function

--- index.js
+++ index.js
@@ -18,9 +18,20 @@ app.get("/files/:filename", function(req, res){
     })
 })
 
+app.get("/edit/:filename", function(req, res){
+    res.render("edit", {filename:req.params.filename})
+})
+
 app.post("/create", function(req, res){
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){})
     res.redirect("/")
+})
+
+app.post("/edit", function(req, res){
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
+        res.redirect("/")
+    })
+    console.log(req.body)
 })
 
 
